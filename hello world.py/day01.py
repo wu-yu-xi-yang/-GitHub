76b3151208cc224bae28d1f00cbfd493c570f516
wu-yu-xi-yang <2820968017@qq.com>
2026-04-06 14:50:10 +0800

    第三次尝试gaoshou

--- hello world.py/day01.py
+++ hello world.py/day01.py
@@ -1,7 +1,3 @@
 print("hello world")
-<<<<<<< HEAD
 print("I love you")
-print("you are stupid")
-=======
-print("I love you")
->>>>>>> da492010b5edf984c43564bf1c9dd2cbea542b32
+print("you are smart")
